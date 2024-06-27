0ab0bf129b83e07529972dbda791de4d16e4ed9c
Kanishka Gupta <0231csai046@niet.co.in>
2024-06-27 13:28:18 +0530

    Update gif.py

--- gif.py
+++ gif.py
@@ -1,6 +1,6 @@
 from moviepy.editor import *
 
-video_clip = VideoFileClip("video_for_task_1.mp4")
+video_clip = VideoFileClip("VID.mp4")
 video_clip = video_clip.subclip(0,6)
 video_clip.write_gif("Task_01.gif",fps = 10)
 gif_video_clip = VideoFileClip("Task_01.gif")
